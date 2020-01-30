1e27bd5f337100251c5c434bbca1ddfc119418f9
segwuonwu <segwuonwu@gmail.com>
2020-01-30 13:42:34 -0800

    displays quotes but need to style the page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log("👀")
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("form").addEventListener("submit", function(e) {
+        // prevent default html from action
+        e.preventDefault();
+
+        // storing the api in a veriable
+        let requestAPI = "https://api.kanye.rest";
+
+        //requesting a fetch
+        fetch(requestAPI)
+            .then(function(respondData) {
+                let quotes = respondData.json();
+                console.log(quotes);
+                return quotes;
+            })
+            .then(function(display) {
+                //displaying the quotes
+                let result = display.quote;
+                let pTag = document.createElement("p");
+                pTag.append(result);
+                let div = document.getElementById("result");
+                div.appendChild(pTag);
+
+                //removing qoutes after a click
+                if (div.hasChildNodes()) {
+                    div.removeChild(div.childNodes[0]);
+                }
+                //displaying the next image
+                div.appendChild(pTag);
+            })
+
+    });
+})
